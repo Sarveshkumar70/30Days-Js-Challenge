ae64f76336054816d6c4fa0d4653bb7c574e0756
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-07-19 14:05:22 +0000

    Arrays

---
+++ Day6_Arrays/Day_6.js
@@ -0,0 +1,60 @@
+// ### Activity 1: Array Creation and Access
+
+// NOTE : EVERY TASK HAS ITS OWN ARRAY, TO MAINTAIN readability, easiness. 
+
+// Task 1:
+let myArray1 = [1, 2, 3, 4, 5];
+// console.log(myArray1);
+
+// Task 2:
+// let myArray2 = ["Ram", "Sham", "Pappu", "Pinky", "Mamta"];
+// console.log(myArray2[0]);
+
+// let lastValue = myArray2[myArray2.length-1];  // for accessing last element
+// console.log(lastValue);
+
+
+// ### Activity 2: Array Methods (Basic)
+
+// Task 3: 
+
+// let myArray3 = [2,4,6,8,10];
+// myArray3.push(12);  // adding new element
+// console.log(myArray3);
+
+
+// Task 4:
+
+// let myArray4 = [2,4,6,8,10];
+// myArray4.pop();
+// console.log(myArray4);
+
+
+// Task 5:
+
+// let myArray5 = [2,4,6,8,10];
+// myArray5.shift();
+// console.log(myArray5);
+
+
+// Task 6:
+
+// let myArray6 = [1,2,3,4,5];
+// myArray6.unshift(6);
+// console.log(myArray6);
+
+
+
+// ### Activity 3: Array Methods (Intermediate)
+
+
+
+// ### Activity 4: Array Iteration
+
+// Task 10:
+// let myArray10 = [11,12,13,14,15,16];
+
+// for(let i =0; i < myArray10.length ; i++){
+//     console.log(myArray10[i]);
+// }
+
