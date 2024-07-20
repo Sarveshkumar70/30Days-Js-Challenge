8847dd744277df565b3a2c224a326c534d6bfc50
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-07-20 07:39:14 +0000

    updated Array

--- Day6_Arrays/Day_6.js
+++ Day6_Arrays/Day_6.js
@@ -1,6 +1,5 @@
 // ### Activity 1: Array Creation and Access
 
-// NOTE : EVERY TASK HAS ITS OWN ARRAY, TO MAINTAIN readability, easiness. 
 
 // Task 1:
 let myArray1 = [1, 2, 3, 4, 5];
@@ -49,18 +48,20 @@ let myArray1 = [1, 2, 3, 4, 5];
 
 // Task 7: 
 // let oldArray = [3,6,9,12,15]
-// let newArray = oldArray.map( (numbers) => numbers * 2 )
+// let newArray = oldArray.map( (numbers) => numbers * 2 ) //doubling each number
 // console.log(newArray);
 
 
-// Task 8
-
+// Task 8:
+// let myArray8 = [1,2,3,4,5,6,7,8];
+// let newArray8 = myArray8.filter( (number) => (number % 2 == 0) ) //filtering the even numbers
+// console.log(newArray8);
 
 
 // Task 9:
 // let array8 = [1,2,3,4,5,6,7,8,9,10]
 
-// let sumOfElements = array8.reduce( (acc, currentVal) =>  {
+// let sumOfElements = array8.reduce( (acc, currentVal) =>  {  // sum of all numbers
 //     return (acc + currentVal);
 // }, 0);
 // console.log(sumOfElements);
@@ -77,9 +78,38 @@ let myArray1 = [1, 2, 3, 4, 5];
 
 
 // Task 11:
+// let array11 = [10, 20, 30, 40, 50];
+// array11.forEach(function(numbers){
+//     console.log(numbers);
+// })
 
-let array11 = [10, 20, 30, 40, 50];
 
-array11.forEach(function(numbers){
-    console.log(numbers);
-})
+// ### Activity 5: Multi-dimensional Arrays
+// Task 12:
+let twoDimArray = [
+    [4,5,6],
+    [1,2,3],
+    [7,8,9]
+]
+
+// console.log("The entire Matrix is:");
+
+// // console.log(twoDimArray);
+
+// // for more readable output
+// // for (let index = 0; index < twoDimArray.length; index++) {
+// //     console.log(twoDimArray[index]);
+// // }
+
+// // or
+// for(let row of twoDimArray){
+//     console.log(row);
+// }
+
+
+// Task 13:
+// let element1 = twoDimArray[2][2]; 
+// console.log(element1);
+
+// let element2 =twoDimArray[1][2];
+// console.log(element2);
