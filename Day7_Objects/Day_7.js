eccfeecd9f5bfd075950fa1f309e6ddc49ce05b7
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-07-20 08:11:20 +0000

    Object

---
+++ Day7_Objects/Day_7.js
@@ -0,0 +1,19 @@
+// ## Activity 1: Object Creation and Access
+
+// Task 1:
+let myBook = {
+    title : "The Complete Novels of SHERLOCK HOLMES ",
+    author : "Sir Arthur Conan Doyle",
+    year : 2022
+}
+
+// console.log(myBook);
+
+
+// Task 2:
+// console.log(myBook.title) 
+
+// console.log(myBook["author"]); // another method for accessing
+
+
+// ##  Activity 2: Object Methods
