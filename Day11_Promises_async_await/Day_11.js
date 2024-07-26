b36e8928dc29d7e38112950f9c0e0f4da3de5c08
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-07-26 13:23:38 +0000

    Task 3

--- Day11_Promises_async_await/Day_11.js
+++ Day11_Promises_async_await/Day_11.js
@@ -16,25 +16,78 @@
 
 
 // Task 2:
-const promiseTwo = new Promise(function (resolve,reject) {
-    let theErrorIs = true;
+// const promiseTwo = new Promise(function (resolve,reject) {
+//     let theErrorIs = true;
 
-    if (!theErrorIs) {
-        resolve({user : "Shaktimaan" , Passion : "Protecting people"})  
-    } else{
-        reject("You got an error");
-    }
+//     if (!theErrorIs) {
+//         resolve({user : "Shaktimaan" , Passion : "Protecting people"})  
+//     } else{
+//         reject("You got an error");
+//     }
+// })
+
+// promiseTwo.then(function (user) {
+//     console.log(user);
+//     return user.Passion;
+// })
+// .then((user2)=>{  // return value gets passed here
+//     console.log(user2);
+// })
+// .catch((err)=>{  // reject
+//     setTimeout(()=>{
+//         console.log(err);
+//     },2000)
+// })
+
+
+// ### **Activity 2: Chaining Promises**
+
+// Task 3:
+
+function fetchUserData() {
+
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve({is : 1,name: "John" });
+        }, 1000);   
+    });
+}
+
+function fetchUserPosts(userId) {
+
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve(['Post1','Post2','Post3']);
+        }, 1500);   
+    });
+}
+
+function fetchUserComments(postId) {
+
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve(['comment1', 'comment2']);
+        }, 1000);   
+    });
+}
+
+console.log('starting data fetch');
+
+fetchUserData().
+then((user)=>{
+    console.log('Fetched user: ',user.name );
+    return fetchUserPosts(user.id);
+})
+.then((posts)=>{
+    console.log(`Fetched posts`, posts.length )
+    return fetchUserComments(posts[0]);
+})
+.then((comments)=>{
+    console.log(`fetched comments`, comments.length);
+    console.log("All data fetched correctly");
+})
+.catch(()=>{
+    console.log("An error occurred",error);
 })
 
-promiseTwo.then(function (user) {
-    console.log(user);
-    return user.Passion;
-})
-.then((user2)=>{  // return value gets passed here
-    console.log(user2);
-})
-.catch((err)=>{  // reject
-    setTimeout(()=>{
-        console.log(err);
-    },2000)
-})
+console.log('Data fetched initiated, waiting for results.....');
