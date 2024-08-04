a4be299c8eb3e6dfaae9546b9b09c3269b735821
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-08-04 13:15:32 +0000

    Linked list

--- Day17_data_structures/Day_17.js
+++ Day17_data_structures/Day_17.js
@@ -1,12 +1,12 @@
 // ### **Activity 1: Linked List**
 
 // Task 1:
-// class Node {
-//     constructor(value){
-//         this.value = value; // assign the values passed to constructor
-//         this.next = null; // reference to the next node , initially set to null
-//     }
-// }
+class Node {
+    constructor(data){
+        this.data = data; // assign the values passed to constructor
+        this.next = null; // reference to the next node , initially set to null
+    }
+}
 
 // // assigning values
 // const node1 = new Node(10)
@@ -24,11 +24,81 @@
 
 // Task 2:
 class LinkedList {
+    constructor(){
+        this.head = null;
+    }
     
+    // Add a node to the end of the list
+    add(data){
+        const newNode = new Node(data)
+        if (this.head === null) {
+            this.head = newNode;
+        }else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
 
 
+    // Remove a node from the end of the list\
+    remove(){
+        if (this.head === null) {
+            return null; // List is empty
+        }
 
+        if (this.head.next === null) {
+            const removeNode = this.head;
+            this.head = null;
+            return removeNode.data; // Only one node in the list
+        }
+
+        let current = this.head;
+        while (current.next.next !== null) {
+            current = current.next;
+        }
+        const removeNode = current.next;
+        current.next = null;
+        return removeNode.data;
+    }
+
+    // Display all nodes in the list
+    display() {
+        let current = this.head;
+        let result = [];
+        while (current !== null) {
+            result.push(current.data);
+            current = current.next;
+        }
+        console.log(result.join(' -> '));   
+    }
 }
+
+const list = new LinkedList();
+
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+
+list.display(); // 1 -> 2 -> 3 -> 4
+
+console.log(list.remove()); // 4
+list.display(); // 1 -> 2 -> 3
+
+console.log(list.remove()); // 3
+list.display(); // 1 -> 2 
+
+console.log(list.remove()); // 2
+list.display(); // 1 
+
+console.log(list.remove()); // 1
+list.display(); 
+
+
+
 
 
 
