aebac29fac7985052c2e2e22591081d225a57b11
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-08-04 08:23:31 +0000

    update task 1

---
+++ Day17_data_structures/Day_17.js
@@ -0,0 +1,35 @@
+// ### **Activity 1: Linked List**
+
+// Task 1:
+// class Node {
+//     constructor(value){
+//         this.value = value; // assign the values passed to constructor
+//         this.next = null; // reference to the next node , initially set to null
+//     }
+// }
+
+// // assigning values
+// const node1 = new Node(10)
+// const node2 = new Node(20)
+// const node3 = new Node(30)
+
+// // Linking nodes 
+// node1.next = node2
+// node2.next = node3
+
+// node 1's value & reference 
+// console.log(node1.value); // VALUE OF node 1 - 10
+// console.log(node1.next); // reference to next node
+
+
+// Task 2:
+class LinkedList {
+    
+
+
+
+}
+
+
+
+
